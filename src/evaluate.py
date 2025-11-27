43856a627a6356b1e1d7b819144198745a23dd5c
chjulie <julie.charlet@epfl.ch>
2025-11-27 11:24:44 -0800

    evaluation.py: nearest-neighbor resampling

--- src/evaluate.py
+++ src/evaluate.py
@@ -2,18 +2,21 @@ import argparse
 import datetime
 import pandas as pd
 import numpy as np
-from pyresample import geometry, bilinear
+from pyresample import geometry, bilinear, kd_tree
 
 # from scipy.interpolate import RegularGridInterpolator
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
 import matplotlib.pyplot as plt
+import matplotlib.tri as tri
 
 from utils import read_pkl
 from data_constants import OBS_DATA_DIR, PRED_DATA_DIR
 
 
-def get_interpolated_value(lat, lon, interpolation_function):
-    interpolated_value = interpolation_function(np.array([[lat, lon]]))
-    return interpolated_value
+# def get_interpolated_value(lat, lon, interpolation_function):
+#     interpolated_value = interpolation_function(np.array([[lat, lon]]))
+#     return interpolated_value
 
 
 if __name__ == "__main__":
@@ -33,12 +36,14 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     field = args.field
-    input_date = pd.to_datetime(args.datetime, format="%Y-%m-%dT%H:%M:%S")
-    print(f"date: {type(input_date)}, {input_date}")
+    input_date = pd.to_datetime(args.datetime, format="%Y-%m-%dT%H:%M:%S").tz_localize(
+        "utc"
+    )
+    # print(f"date: {type(input_date)}, {input_date}")
 
     obs_path = f"{OBS_DATA_DIR}/{args.datetime[:10].replace('-','')}_verif_eccc_obs.csv"
     pred_path = (
-        f"{PRED_DATA_DIR}/{args.datetime[:10].replace('-','')}_regional_state.pkl"
+        f"{PRED_DATA_DIR}/{args.datetime[:13].replace('-','')}_regional_state.pkl"
     )
 
     # TODO:
@@ -47,43 +52,70 @@ if __name__ == "__main__":
     p_date, p_latitudes, p_longitudes, p_fields = read_pkl(
         pred_path
     )  # 'p' prefix indicate 'predictions'. Lats and Lons are 1D, but not a regular grid (duplicated values).
-    print(
-        f"0: {np.count_nonzero(p_latitudes[p_latitudes == p_latitudes[0]])}, -1: {np.count_nonzero(p_latitudes[p_latitudes == p_latitudes[-1]])}"
-    )
-    print(f" - lats.shape: {p_latitudes.shape}")
-    print(f" - lons.shape: {p_longitudes.shape}")
-    print(
-        " - np.unique(lats, return_counts=True): ",
-        np.unique(p_latitudes, return_counts=True),
-    )
-    print(f" - field: {p_fields['2t'].shape}")
-
+    print(" > read prediction file")
     # plt.scatter(longitudes, p_latitudes, c=fields['2t'])
     # plt.show()
     # obs_df = pd.read_csv(obs_path, converters={'UTC_DATE': lambda x: datetime.datetime.strptime(x, "%Y-%m-%dT%H:%M:%S")})
-    # obs_df = pd.read_csv(obs_path, converters={'UTC_DATE': lambda x: pd.to_datetime(x, format="%Y-%m-%dT%H:%M:%S").tz_localize('utc')})
+    obs_df = pd.read_csv(
+        obs_path,
+        converters={
+            "UTC_DATE": lambda x: pd.to_datetime(
+                x, format="%Y-%m-%dT%H:%M:%S"
+            ).tz_localize("utc")
+        },
+    )
+    print(" > read observations file")
+
     # print(obs_df.shape)
     # print(f'date: {type(obs_df.UTC_DATE[200])}, {obs_df.UTC_DATE[200]}')
-    # print(' ** obs date: ', obs_df.UTC_DATE[200].tzinfo)
-    # print(' ** pred date: ', date.tzinfo)
+    print(" ** obs date: ", obs_df.UTC_DATE[200])
+    print(" ** pred date: ", p_date)
+    print(" ** input date: ", input_date)
     # print(obs_df.UTC_DATE[200] - date)
-    # obs_df = obs_df[obs_df.UTC_DATE == date]
-    # print(obs_df.shape)
+    obs_df = obs_df[obs_df.UTC_DATE == input_date]
+    print(" obs_df shape: ", obs_df.shape)
     s_longitudes = obs_df.x.values  # 's' prefix indicates 'station observation'
     s_latitudes = obs_df.y.values  # 's' prefix indicates 'station observation'
+    print("\n")
+    print(f" * s_lon: {s_longitudes.shape}")
+    print(f" * s_lat: {s_latitudes.shape}")
+    print(f" - unique stn_id: {np.count_nonzero(np.unique(obs_df['STN_ID']))}")
+
     # - get station localisation.
 
     # - y_pred: Interpolate prediction to station localisation (bilinear interpolation)
     # pyresample
     pred_grid = geometry.SwathDefinition(lons=p_longitudes, lats=p_latitudes)
     station_loc = geometry.SwathDefinition(lons=s_longitudes, lats=s_latitudes)
-    interpolated_values = bilinear.NumpyBilinearResampler(
-        pred_grid, station_loc, 3000
-    ).resample(p_fields[field])
-    print(
-        f" - interpolated_values: {type(interpolated_values)}, {interpolated_values.shape}"
+    # bilinear_resampler = bilinear.NumpyBilinearResampler(pred_grid, station_loc, 3000)
+    # print(bilinear_resampler)
+    # s_pred = bilinear_resampler.resample(p_fields[field])
+    s_pred = kd_tree.resample_nearest(
+        source_geo_def=pred_grid,
+        data=p_fields[field],
+        target_geo_def=station_loc,
+        radius_of_influence=50000,
     )
-    # - y_obs: get observation value
+    #     pred_grid, station_loc, data=p_fields[field], radius_of_influence=50000
+    # )
+    print("\n")
+    print(f" - interpolated_values: {type(s_pred)}, {s_pred.shape}")
+
+    ## Plot resampled data :
+    fig, ax = plt.subplots(
+        figsize=(6, 6), subplot_kw={"projection": ccrs.PlateCarree()}
+    )
+    ax.coastlines()
+    ax.add_feature(cfeature.BORDERS, linestyle=":")
+    ax.scatter(s_longitudes, s_latitudes, c=s_pred, cmap="RdBu")
+
+    triangulation = tri.Triangulation(p_longitudes, p_latitudes)
+    contour = ax.tricontourf(
+        triangulation, p_fields[field], levels=20, cmap="RdBu", alpha=0.5
+    )  # transform=ccrs.PlateCarree()
+
+    plt.show()
+    # # - y_obs: get observation value
     # => df.columns : STN_ID, LAT, LON, Y_PRED, Y_OBS
     # - compare y_pred and y_obs
     # => df.columns : STN_ID, LAT, LON, Y_PRED, Y_OBS, ERR_0, ..., ERR_N
--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,16 @@
 import pickle
 import pandas as pd
 
+
 def read_pkl(path):
 
     with open(path, "rb") as f:
         domain_state = pickle.load(f)
         print(f" PRED DATE: {type(domain_state["date"])}, {domain_state["date"]}")
-        date = pd.to_datetime(domain_state["date"]).tz_localize('utc')
+        date = pd.to_datetime(domain_state["date"])  # .tz_localize('utc')
         print(f" PD PRED DATE: {type(date)}, {date}")
         latitudes = domain_state["latitudes"]
         longitudes = domain_state["longitudes"]
         fields = domain_state["fields"]
-    
+
     return date, latitudes, longitudes, fields
